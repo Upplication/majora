07650876f80d5586ca7626509486cf0df41b87de
Miguel Molina <hi@mvader.me>
2015-03-25 17:33:53 +0100

    Removed unused tests

--- app/tests/unit/controllers/user-ctrl_spec.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-describe('connect to /register', function () {
-    it('should return TOKEN', function () {
-        var obj = "{success: true; token:1234; expitation: 2015-01-30}";
-        expect(1+1).toBe(2);
-    });
-});
