f83bbd832a409c9b73e7ffe2cdedecbf32ab33a7
Miguel Molina <hi@mvader.me>
2015-04-22 16:43:06 +0200

    Small modifications to template schema

--- api/models/template.js
+++ api/models/template.js
@@ -6,27 +6,27 @@ var mongoose = require('mongoose'),
 /**
  * Template with:
  * - name (string)
- * - snapshots (list urls)
+ * - snapshots (urls list)
  * - version (int)
  * - author (reference email)
  */
 var templateSchema = new Schema({
-	name: String,
+    name: String,
     author: String,
     snapshots: [String],
     version: Number
 });
 
 /**
- * return as a JSON without special mongo fields like _id and __v
+ * Returns the template as JSON with only the required fields
  */
-templateSchema.methods.toJson = function(){
-    this._id = undefined;
-    this.__v = undefined;
-    // dont work: http://stackoverflow.com/questions/4486926/delete-a-key-from-a-mongodb-document-using-mongoose
-    delete this['_id'];
-    delete this.__v;
-    return this;
+templateSchema.methods.toJson = function () {
+    return {
+        name: this.name,
+        snapshots: this.snapshots,
+        version: this.version,
+        author: this.author
+    };
 };
 
 /**
@@ -35,22 +35,20 @@ templateSchema.methods.toJson = function(){
  * @param {Function} callback Callback function
  */
 templateSchema.statics.findAll = function (callback) {
-    if (callback){
+    if (callback) {
         this.find().exec(callback);
-    }
-    else {
+    } else {
         return this.find();
     }
 };
 
 /**
- * Finds a unique template with the given name
- * @param {String}   name    Name
+ * Finds a template with the given name
+ * @param {String}   name     Name
  * @param {Function} callback Callback function
  */
 templateSchema.statics.findByName = function (name, callback) {
     this.findOne({name: new RegExp(name, 'i')}, callback);
 };
-
 
 module.exports = mongoose.model('Template', templateSchema);
